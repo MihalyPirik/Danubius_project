fb886b33c9e6ae1bf4d5a208351c03edf435e6dd
Mihaly Pirik <misipirik@gmail.com>
2024-04-20 19:02:09 +0200

    connect to database

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error)
+})
+database.once('connected', () => {
+  console.log('Database Connected');
+})
 
 const port = 3000;
 
-mongoose.set('strictQuery', true);
-
 const app = express();
-
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server Started at ${port}`)
+app.listen(3000, () => {
+  console.log(`Server Started at ${3000}`)
 })
